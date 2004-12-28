47f28f5efd7ab44005a82424c8a237f49fd65c6a
Albert Astals Cid <tsdgeos@terra.es>
2004-12-28 00:17:37 +0000

    Poland and Austria maps, thanks Mikolaj Machowski
    svn path=/trunk/kdenonbeta/kgeography/; revision=373630

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,10 @@ int main(int argc, char *argv[])
 {
 	KAboutData about("kgeography", I18N_NOOP("KGeography"), "0.3.1", I18N_NOOP("A geography learning program"), KAboutData::License_GPL, 0, 0, "http://kgeography.berlios.de", "tsdgeos@terra.es");
 	about.addAuthor("Albert Astals Cid", I18N_NOOP("Programmer and designer"), "tsdgeos@terra.es");
-	about.addAuthor("Yann Verley", I18N_NOOP("Contributed some maps and map generation tools"), "yann.verley@free.fr");
-	about.addAuthor("Justin Morgan", I18N_NOOP("Contributed North and Central America map"));
-	about.addAuthor("Maurizio Paolini", I18N_NOOP("Contributed Italy map"));
+	about.addCredit("Yann Verley", I18N_NOOP("Contributed some maps and map generation tools"), "yann.verley@free.fr");
+	about.addCredit("Justin Morgan", I18N_NOOP("Contributed North and Central America map"));
+	about.addCredit("Maurizio Paolini", I18N_NOOP("Contributed Italy map"));
+	about.addCredit("Mikolaj Machowski", I18N_NOOP("Contributed Poland and Austria map"));
 	about.addCredit(I18N_NOOP("Sodipodi flags collection"), I18N_NOOP("Got some flags from it"), 0, "http://www.sodipodi.com/index.php3?section=clipart/flags");
 	KCmdLineArgs::init(argc, argv, &about);
 	KApplication app;
