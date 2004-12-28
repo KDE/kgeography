c9d8a1cdb4e7bcf009130dce860832d5bd777b64
Albert Astals Cid <tsdgeos@terra.es>
2004-12-28 20:11:18 +0000

    Update
    svn path=/trunk/kdenonbeta/kgeography/; revision=373822

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	about.addCredit("Yann Verley", I18N_NOOP("Contributed some maps and map generation tools"), "yann.verley@free.fr");
 	about.addCredit("Justin Morgan", I18N_NOOP("Contributed North and Central America map"));
 	about.addCredit("Maurizio Paolini", I18N_NOOP("Contributed Italy map"));
-	about.addCredit("Mikolaj Machowski", I18N_NOOP("Contributed Poland and Austria map"));
+	about.addCredit("Mikolaj Machowski", I18N_NOOP("Contributed Norway, Poland and Austria map"));
 	about.addCredit(I18N_NOOP("Sodipodi flags collection"), I18N_NOOP("Got some flags from it"), 0, "http://www.sodipodi.com/index.php3?section=clipart/flags");
 	KCmdLineArgs::init(argc, argv, &about);
 	KApplication app;
