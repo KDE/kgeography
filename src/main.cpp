fb6548aef69be5a8f1c9ed83b7d14cebcc28c061
Albert Astals Cid <tsdgeos@terra.es>
2005-04-20 19:05:08 +0000

    Asia map by Justin Morgan CCMAIL:feedback@corfizz.com
    svn path=/trunk/kdenonbeta/kgeography/; revision=406786

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,13 @@ int main(int argc, char *argv[])
 {
 	KAboutData about("kgeography", I18N_NOOP("KGeography"), "0.3.1", I18N_NOOP("A geography learning program"), KAboutData::License_GPL, 0, 0, "http://kgeography.berlios.de", "tsdgeos@terra.es");
 	about.addAuthor("Albert Astals Cid", I18N_NOOP("Programmer and designer"), "tsdgeos@terra.es");
-	about.addCredit("Yann Verley", I18N_NOOP("Contributed some maps and map generation tools"), "yann.verley@free.fr");
-	about.addCredit("Mikolaj Machowski", I18N_NOOP("Contributed Norway, Poland and Austria map"));
-	about.addCredit("Melody Fink", I18N_NOOP("Contributed China map"));
-	about.addCredit("Justin Morgan", I18N_NOOP("Contributed North and Central America map and Brazil map"));
-	about.addCredit("Maurizio Paolini", I18N_NOOP("Contributed Italy map"));
 	about.addCredit("Danny Allen", I18N_NOOP("Small refinements"), "danny@dannyallen.co.uk");
 	about.addCredit("Giuseppe Caruso", I18N_NOOP("Italy flags and Italy by provinces map"), "caruso@pietrobo.com");
+	about.addCredit("Melody Fink", I18N_NOOP("Map contributor"));
+	about.addCredit("Mikolaj Machowski", I18N_NOOP("Map contributor"));
+	about.addCredit("Justin Morgan", I18N_NOOP("Map contributor"));
+	about.addCredit("Maurizio Paolini", I18N_NOOP("Map contributor"));
+	about.addCredit("Yann Verley", I18N_NOOP("Contributed some maps and map generation tools"), "yann.verley@free.fr");
 	about.addCredit(I18N_NOOP("Sodipodi flags collection"), I18N_NOOP("Got some flags from it"), 0, "http://www.sodipodi.com/index.php3?section=clipart/flags");
 	KCmdLineArgs::init(argc, argv, &about);
 	KApplication app;
