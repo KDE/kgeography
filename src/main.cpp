2f7f287aaec68b23c3e6d008fa6124e9b861b1da
Albert Astals Cid <tsdgeos@terra.es>
2006-01-19 21:53:35 +0000

    List of map contributors is growing too much :-/
    svn path=/trunk/KDE/kdeedu/kgeography/; revision=500283

--- src/main.cpp
+++ src/main.cpp
@@ -19,15 +19,6 @@ int main(int argc, char *argv[])
 {
 	KAboutData about("kgeography", I18N_NOOP("KGeography"), "0.5", I18N_NOOP("A geography learning program"), KAboutData::License_GPL, "© 2004-2005 Albert Astals Cid", 0, "http://kgeography.berlios.de");
 	about.addAuthor("Albert Astals Cid", I18N_NOOP("Programmer and designer"), "tsdgeos@terra.es");
-	about.addCredit("Danny Allen", I18N_NOOP("Small refinements"), "danny@dannyallen.co.uk");
-	about.addCredit("Giuseppe Caruso", I18N_NOOP("Italy flags and Italy by provinces map"), "caruso@pietrobo.com");
-	about.addCredit("Melody Fink", I18N_NOOP("Map contributor"));
-	about.addCredit("Mikolaj Machowski", I18N_NOOP("Map contributor"));
-	about.addCredit("Ed Montgomery", I18N_NOOP("Map contributor"));
-	about.addCredit("Justin Morgan", I18N_NOOP("Map contributor"));
-	about.addCredit("Frank Mutsaers", I18N_NOOP("Map contributor"));
-	about.addCredit("Maurizio Paolini", I18N_NOOP("Map contributor"));
-	about.addCredit("Yann Verley", I18N_NOOP("Contributed some maps and map generation tools"), "yann.verley@free.fr");
 	about.addCredit(I18N_NOOP("Sodipodi flags collection"), I18N_NOOP("Got some flags from it"), 0, "http://www.sodipodi.com/index.php3?section=clipart/flags");
 	KCmdLineArgs::init(argc, argv, &about);
 	KApplication app;
