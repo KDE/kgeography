048bac200906475ae39c727fa5a418e64afd3099
Laurent Garnier <lauranger@free.fr>
2008-09-28 20:17:28 +0000

    Sums up input files errors into a unique dialog instead of a potential dialog war. Adds a slider in number of question asking. Part 2
    svn path=/trunk/KDE/kdeedu/kgeography/; revision=865735

---
+++ src/integerinputdialog.cpp
@@ -0,0 +1,89 @@
+/***************************************************************************
+ *   Copyright (C) 2004-2008 by Albert Astals Cid                          *
+ *   aacid@kde.org                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include "integerinputdialog.h"
+
+#include <qboxlayout.h>
+#include <qlabel.h>
+#include <qslider.h>
+#include <qspinbox.h>
+
+IntegerInputDialog::IntegerInputDialog(QWidget *parent, const QString &title, const QString &question,
+				       int from, int upto, int byDefault)
+	: KDialog(parent)
+{
+	setCaption(title);
+	setButtons(KDialog::Ok | KDialog::Cancel);
+
+	QWidget *container = new QWidget(this);
+	setMainWidget(container);
+	QVBoxLayout *vbox = new QVBoxLayout(container);
+	QLabel *questionLbl = new QLabel(question);
+	vbox->addWidget(questionLbl);
+	QHBoxLayout *hbox = new QHBoxLayout();
+	vbox->addLayout(hbox);
+
+	_slider = new QSlider(Qt::Horizontal);
+	_slider->setRange(from, upto);
+	int value = ( from <= byDefault && byDefault <= upto ) ? byDefault : from;
+	_slider->setValue(value);
+	connect(_slider, SIGNAL(valueChanged(int)), this, SLOT(sliderValueChanged(int)));
+	hbox->addWidget(_slider);
+
+	_spinBox = new QSpinBox();
+	_spinBox->setRange(from, upto);
+	_spinBox->setValue(value);
+	connect(_spinBox, SIGNAL(valueChanged(int)), this, SLOT(spinboxValueChanged(int)));
+	hbox->addWidget(_spinBox);
+}
+
+int IntegerInputDialog::value() const
+{
+	return _slider->value();
+}
+
+void IntegerInputDialog::setValue(int newValue)
+{
+	if ( _slider->value() != newValue )
+		_slider->setValue(newValue);
+	if ( _slider->value() != newValue )
+		_slider->setValue(newValue);
+}
+
+void IntegerInputDialog::sliderValueChanged(int newValue)
+{
+	if ( _spinBox->value() != newValue )
+		_spinBox->setValue(newValue);
+}
+
+void IntegerInputDialog::spinboxValueChanged(int newValue)
+{
+	if ( _slider->value() != newValue )
+		_slider->setValue(newValue);
+}
+
+int IntegerInputDialog::GetInteger(QWidget *parent, const QString &title, const QString &question,
+				   int from, int upto, int byDefault, bool *rOK)
+{
+	int ret = -1;
+	bool ok = false;
+	IntegerInputDialog dialog(parent, title, question, from, upto, byDefault);
+	if ( dialog.exec() == QDialog::Accepted )
+	{
+		ok = true;
+		ret = dialog.value();
+	}
+	if ( rOK != NULL ) *rOK = ok;
+	return ret;
+
+}
+
+#include "integerinputdialog.moc"
+
---
+++ src/integerinputdialog.h
@@ -0,0 +1,45 @@
+
+#include <kdialog.h>
+/***************************************************************************
+ *   Copyright (C) 2004-2008 by Albert Astals Cid                          *
+ *   aacid@kde.org                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef _INTEGER_INPUT_DIALOG_H
+#define _INTEGER_INPUT_DIALOG_H
+
+#include <kdialog.h>
+
+class QSpinBox;
+class QSlider;
+
+class IntegerInputDialog : public KDialog
+{
+Q_OBJECT
+	public:
+		IntegerInputDialog(QWidget *parent, const QString &title, const QString &question,
+				   int from, int upto, int byDefault = -1);
+		static int GetInteger(QWidget *parent, const QString &title, const QString &question,
+				      int from, int upto, int byDefault, bool *rOK);
+
+		int value() const;
+
+	public slots:
+		void setValue(int newValue);
+
+	private slots:
+		void sliderValueChanged(int newValue);
+		void spinboxValueChanged(int newValue);
+
+	private:
+		QSpinBox *_spinBox;
+		QSlider  *_slider;
+};
+
+#endif // def _INTEGER_INPUT_DIALOG_H
+
