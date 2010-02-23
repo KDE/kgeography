6d2d7b19bc4be75eee8677b0d6c1596827ab45c0
Albert Astals Cid <tsdgeos@terra.es>
2010-02-23 20:44:39 +0000

    backport r1094585 | lauranger | 2010-02-22 23:06:51 +0000 (Mon, 22 Feb 2010) | 1 line
    Change default number of questions to the max. BUG:228108
    
    svn path=/branches/KDE/4.4/kdeedu/kgeography/; revision=1095223

--- src/kgeography.cpp
+++ src/kgeography.cpp
@@ -223,7 +223,7 @@ int kgeography::askQuestionNumber(int upto, bool *rOK)
 	bool ok;
 	i = IntegerInputDialog::GetInteger(NULL, i18n("Number of Questions"),
 					   i18n("How many questions do you want? (1 to %1)", upto),
-					   1, upto, 1, &ok);
+					   1, upto, upto, &ok);
 	if ( rOK != NULL ) *rOK = ok;
 	return i;
 }
