3ca10237c4adc86db0ef1a04fafe340c4bf9c688
Albert Astals Cid <aacid@kde.org>
2012-09-24 15:05:55 +0200

    Extract contents from the kgm on Messages.sh time
    Instead of having to run a script offline and commit the translations
    Much more fool proof :-)

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 $EXTRACTRC *.ui *.rc *.kcfg >> rc.cpp
+
+cd ../data
+for i in *kgm; do $EXTRACTRC --context="$i" --tag=name --tag=capital --tag=divisionsName $i; done >> ../src/rc.cpp
+for i in *kgm; do $EXTRACTRC --context="@title:group" --tag=divisionToCapitalPattern --tag=capitalToDivisionPattern $i; done >> ../src/rc.cpp
+for i in *kgm; do $EXTRACTRC --context="@title" --tag=capitalToDivisionTitle --tag=divisionToCapitalTitle $i; done >> ../src/rc.pp
+cd ../src
+
 $XGETTEXT *.cpp -o $podir/kgeography.pot
