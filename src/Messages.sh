a28a8d84095f31f840d920a4ce48b135027d2b3c
Pino Toscano <pino@kde.org>
2012-09-25 10:10:26 +0200

    Messages.sh: fix rc.cpp filename; remove it when done

--- src/Messages.sh
+++ src/Messages.sh
@@ -4,7 +4,8 @@ $EXTRACTRC *.ui *.rc *.kcfg >> rc.cpp
 cd ../data
 for i in *kgm; do $EXTRACTRC --context="$i" --tag=name --tag=capital --tag=divisionsName $i; done >> ../src/rc.cpp
 for i in *kgm; do $EXTRACTRC --context="@title:group" --tag=divisionToCapitalPattern --tag=capitalToDivisionPattern $i; done >> ../src/rc.cpp
-for i in *kgm; do $EXTRACTRC --context="@title" --tag=capitalToDivisionTitle --tag=divisionToCapitalTitle $i; done >> ../src/rc.pp
+for i in *kgm; do $EXTRACTRC --context="@title" --tag=capitalToDivisionTitle --tag=divisionToCapitalTitle $i; done >> ../src/rc.cpp
 cd ../src
 
 $XGETTEXT *.cpp -o $podir/kgeography.pot
+rm -f rc.cpp
