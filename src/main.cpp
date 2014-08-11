939d61501f25e4fffd52c4c53411c23c9692dffd
Albert Astals Cid <aacid@kde.org>
2014-08-11 22:03:01 +0200

    Don't forget to call KLocalizedString::setApplicationDomain

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 
 int main(int argc, char *argv[])
 {
+	KLocalizedString::setApplicationDomain("kgeography");
+
 	QApplication app(argc, argv);
 	KAboutData about("kgeography", i18n("KGeography"), "0.8.1", i18n("A Geography learning program"), KAboutLicense::GPL, i18n("© 2004-2005 Albert Astals Cid"), QString(), "http://userbase.kde.org/KGeography");
 	about.addAuthor(i18n("Albert Astals Cid"), i18n("Programmer and designer"), "aacid@kde.org");
